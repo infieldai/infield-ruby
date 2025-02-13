3391cfa780a300ebdd795b1502f067acb7572846
Brian Boylen <bbbrianboylen@gmail.com>
2025-02-13 15:53:16 -0500

    Automatically hook into rails deprecation warnings

--- lib/infield/rails.rb
+++ lib/infield/rails.rb
@@ -3,9 +3,19 @@
 module Infield
   class Railtie < Rails::Railtie
     initializer 'infield.deprecation_warnings', after: 'active_support.deprecation_behavior' do |_app|
-      ActiveSupport::Notifications.subscribe('deprecation.rails') do |_name, _start, _finish, _id, payload|
+      infield_lambda = lambda do |message, callstack, *args|
+        Infield::DeprecationWarning.log(message, callstack: callstack, validated: true)
+      end
 
-        Infield::DeprecationWarning.log(payload[:message], callstack: payload[:callstack], validated: true)
+      # Rails >= 7.0 makes it so that there are named deprecators that can have their own behavior
+      if Rails.application.respond_to?(:deprecators)
+        Rails.application.deprecators.each do |deprecator|
+          current    = Array(deprecator.behavior)
+          deprecator.behavior = [infield_lambda, *current].uniq
+        end
+      else
+        current_behaviors = Array(ActiveSupport::Deprecation.behavior)
+        ActiveSupport::Deprecation.behavior = [infield_lambda, *current_behaviors].uniq
       end
     end
   end
