827a78c68bc7e4d6c00a453ff7113749a4a5fd83
Brian Boylen <bbbrianboylen@gmail.com>
2025-02-17 14:46:52 -0500

    Make gem automatically enable active_support.report_deprecations

--- lib/infield/rails.rb
+++ lib/infield/rails.rb
@@ -2,20 +2,24 @@
 
 module Infield
   class Railtie < Rails::Railtie
-    initializer 'infield.deprecation_warnings', after: 'active_support.deprecation_behavior' do |_app|
-      infield_lambda = lambda do |message, callstack, *args|
+    initializer 'infield.enable_report_deprecations', before: 'active_support.deprecation_behavior' do |app|
+      app.config.active_support.report_deprecations = true
+    end
+
+    initializer 'infield.deprecation_warnings', after: 'active_support.deprecation_behavior' do |app|
+      infield_logger = lambda do |message, callstack, *_args|
         Infield::DeprecationWarning.log(message, callstack: callstack, validated: true)
       end
 
       # Rails >= 7.0 makes it so that there are named deprecators that can have their own behavior
-      if Rails.application.respond_to?(:deprecators)
-        Rails.application.deprecators.each do |deprecator|
-          current    = Array(deprecator.behavior)
-          deprecator.behavior = [infield_lambda, *current].uniq
+      if app.respond_to?(:deprecators)
+        app.deprecators.each do |deprecator|
+          current_behaviors = Array(deprecator.behavior)
+          deprecator.behavior = [infield_logger, *current_behaviors].uniq
         end
       else
         current_behaviors = Array(ActiveSupport::Deprecation.behavior)
-        ActiveSupport::Deprecation.behavior = [infield_lambda, *current_behaviors].uniq
+        ActiveSupport::Deprecation.behavior = [infield_logger, *current_behaviors].uniq
       end
     end
   end
